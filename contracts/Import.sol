ad7e8d1a0436b55d3718d4a57cae15b6201fd658
Alex Forshtat <forshtat1@gmail.com>
2024-12-23 15:04:07 +0100

    Bring over samples tests and test utils

---
+++ contracts/Import.sol
@@ -0,0 +1,7 @@
+import "@account-abstraction/contracts/core/EntryPoint.sol";
+import "@account-abstraction/contracts/core/EntryPointSimulations.sol";
+
+contract Import {
+    EntryPoint public ep;
+    EntryPointSimulations public eps;
+}
