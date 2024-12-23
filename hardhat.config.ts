5367da16acb95b3bd4b88384e6c72cb431a05900
Alex Forshtat <forshtat1@gmail.com>
2024-12-23 17:07:31 +0100

    Bring over hardhat and github actions configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomiclabs/hardhat-waffle'
 import '@typechain/hardhat'
+import { HardhatUserConfig } from 'hardhat/config'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-};
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.24',
+        settings: {
+          evmVersion: 'cancun',
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000000
+          }
+        }
+      }
+    ]
+  }
+}
 
-export default config;
+export default config
